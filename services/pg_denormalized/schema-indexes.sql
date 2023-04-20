68cd0912c7d89f3084b524fff6ab49c80cdda416
Judith Zhao <judithzhao@outlook.com>
2023-04-20 16:16:56 -0700

    Denormalized Indexes seem work but queries are not entirely correct

--- services/pg_denormalized/schema-indexes.sql
+++ services/pg_denormalized/schema-indexes.sql
@@ -1,4 +1,5 @@
 CREATE INDEX gin_jsonb_tags ON tweets_jsonb USING gin((data->'entities'->'hashtags'));
 CREATE INDEX gin_jsonb_ext_tags ON tweets_jsonb USING gin((data->'extended_tweet'->'entities'->'hashtags'));
 
-CREATE INDEX gin_tweet_text ON tweets_jsonb USING gin(to_tsvector('english', coalesce(data->'extended_tweet'->>'full_text'), data->>'text'));
+-- Q4-Q5
+CREATE INDEX gin_tweet_text ON tweets_jsonb USING gin(to_tsvector('english', coalesce(data->'extended_tweet'->>'full_text', data->>'text'))) WHERE (data->>'lang'='en')
