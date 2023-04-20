4c745889f505196d863f39e5506878b331b7ada3
Judith Zhao <judithzhao@outlook.com>
2023-04-19 18:31:12 -0700

    Normalized Somehow Working (>4 secs tho)

--- services/pg_normalized_batch/schema-indexes.sql
+++ services/pg_normalized_batch/schema-indexes.sql
@@ -1,6 +1,9 @@
+-- Q1 and Q2
 CREATE INDEX tag_id_tweets_index ON tweet_tags(tag, id_tweets);
-CREATE INDEX id_tweets_tag_index ON tweet_tags(id_tweets, tag);
+CREATE INDEX id_tweets_tag_index ON tweet_tags(id_tweets, tag); -- (probly Q3)
+
+-- Q3 and Q4
 CREATE INDEX id_tweets_lang_index ON tweets(id_tweets, lang);
 CREATE INDEX lang_index on tweets(lang);
-
+-- (gin)
 CREATE INDEX gin_index ON tweets USING gin(to_tsvector('english',text));
